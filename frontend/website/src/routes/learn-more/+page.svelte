ec679e75408460d3c50f652c4b761c040751225a
KaiFu7k <kaifu7k@gmail.com>
2025-02-24 23:44:07 -0500

    temp button (doesnt work atm im tryna fix tho)

--- frontend/website/src/routes/learn-more/+page.svelte
+++ frontend/website/src/routes/learn-more/+page.svelte
@@ -1,4 +1,26 @@
+<main>
+    <div class=" learnMorePage">
+        <h1> I am learning so much about ForceCoin </h1>
+    </div>
+    <a href="/routes" class="btn">
+        <button>HomePage</button>
+    </a>
+</main>
+<style>
+    .btn {
 
-<div class=" learnMorePage">
-    <h1> I am learning so much about ForceCoin </h1>
-</div>
+
+        background-color: #0066cc;
+        color: white;
+        padding: 10px 60px;
+        border-radius: 10px;
+        text-decoration: none ;
+        font-weight: bold;
+        position: relative;
+        top: 50px;
+    }
+
+    .btn:hover {
+        background-color: #004d99;
+    }
+</style>
