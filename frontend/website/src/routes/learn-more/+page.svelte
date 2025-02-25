c9c57dc137c78bf00911994524b6086661925d32
KaiFu7k <kaifu7k@gmail.com>
2025-02-24 23:47:54 -0500

    button for subpage works (im dumb)

--- frontend/website/src/routes/learn-more/+page.svelte
+++ frontend/website/src/routes/learn-more/+page.svelte
@@ -2,7 +2,7 @@
     <div class=" learnMorePage">
         <h1> I am learning so much about ForceCoin </h1>
     </div>
-    <a href="/routes" class="btn">
+    <a href="/" class="btn">
         <button>HomePage</button>
     </a>
 </main>
